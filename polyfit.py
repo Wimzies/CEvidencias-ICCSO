8c81761c613d986511d3d3f372216e68df1763be
Wimzies <48831777+Wimzies@users.noreply.github.com>
2019-06-04 19:51:08 -0500

    Update polyfit.py

--- polyfit.py
+++ polyfit.py
@@ -36,25 +36,24 @@ def polyfit2(x,y,n):			#declara la función polyfit2 con sus argumentos.
     return prod(prod(inv(prod(trans(A),A)),trans(A)),trans(yT))   #realiza la ecuación completa para el cálculo del ajuste
 print(polyfit2(X,Y,2)) 
 
-x = S.symbols('x') 
-y = S.symbols('y')
+x = S.symbols('x')          #para poder definir la variable x
+y = S.symbols('y')          #para poder definir la variable x
  
 #una vez que se obtienen los valores de ajuste, se realizan los cálculos para representar los puntos de ajuste en la gráfica
 
-y = -3.33 + 0.53 * x -1.94 * pow(x,2) +0.523 * pow(x,3) +0.496 * pow(x,2) #
-f = S.lambdify(x,y,'math')
-yen = f(X)
-print(yen)
-yout= yen.astype(list)
+y = -3.33 + 0.53 * x -1.94 * pow(x,2) +0.523 * pow(x,3) +0.496 * pow(x,2) #con los valores obtenidos, podemos realizar el ajuste. Dependiendo del grado del ajuste, se insertan los datos obtenido multiplicados por esa potencia para hacer el ajuste.
+f = S.lambdify(x,y,'math')  #transforma expresiones de sympy a funciones lambda para realizar cálculos numéricos.
+yen = f(X)                  #aplica el método a los valores de x y los almacena en la variable yen (entrada)
+print(yen)                  #imprime yen para comprobación
+yout= yen.astype(list)      #muestra los valores de entrada de y como lista con el método .astype(list) y los almacena en yout
 
-plt.scatter(X,Y, color = 'orange')
-plt.scatter(X,yout , color='purple')
-plt.show()
-
-
+plt.scatter(X,Y, color = 'orange')      #hace una gráfica de dispersión con los valores de x y y'
+plt.scatter(X,yout , color='purple')    #hace la gráfica de dispersión con los valores de  x y de yout
+plt.show()                  #muestra las gráficas en la interfaz de matplotlib
 
 
-
+#FUNCION PARA GRADO 3
+#A partir de aqui se realiza el mismo procedimiento, según el avance de lo grados de ajuste, se agrega la potencia deseada
 def polyfit3(x,y,n): 
 
     def inv(A): 
@@ -64,7 +63,7 @@ def polyfit3(x,y,n):
     def oneMat(xl,n): 
         return np.ones((xl,n),dtype=int)
     def prod(A,B): 
-        return np.dot(A,B)
+        return np.dot(A,B)                                  #el procedimiento es similar
 
     xlen = len(x)
     ylen = len(y)
@@ -97,9 +96,7 @@ plt.scatter(X,Y, color = 'orange')
 plt.scatter(X,yout , color='purple')
 plt.show()
 
-
-
-
+#FUNCION PARA GRADO 4
 
 def polyfit4(x,y,n): 
 
@@ -116,7 +113,7 @@ def polyfit4(x,y,n):
     ylen = len(y)
     one = np.ones((xlen,n+1),dtype=int)
     c1=one[:,[1]]
-    xT=np.matrix(x)
+    xT=np.matrix(x)                         #Se agrega una nueva potencia para aumentar la matriz
     yT=np.matrix(y)
     c2=xT.getT()
     c3=np.power(c2,2)
